14915b2df8d914564ff0efd1f89126045245138f
Seth Schwartz <sschwartz214@gmail.com>
2018-08-16 15:45:55 -0600

    test for most items

--- spec/requests/api/v1/business_intelligence/merchants/most_items_spec.rb
+++ spec/requests/api/v1/business_intelligence/merchants/most_items_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Merchants API' do
-  xit 'loads a variable number of top merchants ranked by total items sold' do
+  it 'loads a variable number of top merchants ranked by total items sold' do
     merchant_1 = create(:merchant)
     merchant_2 = create(:merchant)
 
@@ -17,10 +17,10 @@ describe 'Merchants API' do
     transaction_1 = create(:transaction, invoice: invoice_1, result: "success")
     transaction_2 = create(:transaction, invoice: invoice_2, result: "success")
 
-    get "/api/v1/merchants/most_items?quantity=2"
+    get "/api/v1/merchants/most_items?quantity=#{2}"
 
     expect(response).to be_successful
     merchants = JSON.parse(response.body)
-    expect(merchants.first[:id]).to eq(merchant_2.id)
+    expect(merchants.first["id"]).to eq(merchant_2.id)
   end
 end
